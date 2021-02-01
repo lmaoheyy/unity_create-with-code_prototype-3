f64558c6d33e91244a4fcda330862d446536707d
Kim Barcelo <kim.barcelo@nbs.systems>
2021-02-01 22:05:26 +0800

    Added animations, particles, and sound effects.

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -9,21 +9,43 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     private bool gameOver = false;
+    private Animator anim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem walkParticle;
+    public AudioClip[] jumpSound;
+    public AudioClip[] crashSound;
+    private AudioSource playerAudio;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
+
+        anim.SetFloat("Speed_f", 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            anim.SetTrigger("Jump_trig");
             isOnGround = false;
+            walkParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound[Random.Range(0,3)], 1.0f);
         }
+
+        /*if (isOnGround)
+        {
+            
+        }
+        else
+        {
+            
+        }*/
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -31,11 +53,18 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            walkParticle.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over");
+            anim.SetFloat("Speed_f", 0.0f);
+            anim.SetBool("Death_b", true);
+            anim.SetInteger("DeathType_int", 1);
             gameOver = true;
+            explosionParticle.Play();
+            walkParticle.Stop();
+            playerAudio.PlayOneShot(crashSound[Random.Range(0, 3)], 1.0f);
         }
         
     }
